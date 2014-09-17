f420adbb758168573bc1693e112817b9b60e387c
Gejgalis, Piotr <Piotr.Gejgalis@sabre.com>
2014-09-17 11:58:53 +0200

    added CommonJS export

--- src/sandbox-post.js
+++ src/sandbox-post.js
@@ -2,6 +2,8 @@
     }
     if (typeof define === "function" && typeof define.amd === "object" && define.amd.vendor !== "dojotoolkit.org") {
         define([], amdFactory);
+    } else if (typeof exports === 'object') {
+        module.exports = amdFactory();
     } else {
         global.izi = amdFactory();
     }
