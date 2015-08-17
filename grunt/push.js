e00dd9d5de7cad219755b11307393ba027c4cb3f
Piotr Gejgalis <Piotr.Gejgalis@smart4aviation.aero>
2015-08-17 12:07:16 +0200

    Fixed release process

--- grunt/push.js
+++ grunt/push.js
@@ -1,6 +1,7 @@
 module.exports.tasks = {
     push: {
         options: {
+            npmTag: 'latest',
             updateConfigs: ['pkg'] // we use `pkg` variable in docs generation
         }
     }
